4a70a51cae1eb92d41de80edbf649c95072c6d86
APT-KK <khoslakabeer527@gmail.com>
2025-09-25 23:51:36 +0530

    fix club class and add club.h

--- club.cpp
+++ club.cpp
@@ -1,41 +1,38 @@
+#include "club.h"
+#include "Member.h"
 #include <iostream>
 using namespace std;
 
-class club{
+club::club(string name, Admin* a) {
+    Clubname = name;
+    admin = a;
+}
 
-    private:
-    string Clubname;
-    Admin* admin;
-    Vector<Members*> members;
-    Vector<Assignment*> assignments;
-
-    public:
-    club(string name,Admin*a){
-        Clubname=name;
-        admin=a;
+void club::listallmembers() {
+    cout << "members of clubs:" << Clubname << "\n";
+    if (members.empty()) {
+        cout << "NO MEMBERS\n";
+        return;
     }
-
-    void listallmembers(){
-        cout<<"members of clubs:"<<Clubname<<"\n";
-        if(members.empty()){
-            cout<<"NO MEMBERS\n";
-            return;
-        }
-        for(int i=0;i<members.getsize();++i){
-            cout<<"-"<<members.get(i)->getName()<<"roll no:"<<members.get(i)->getRoll()<<"\n";
-        }
+    for (int i = 0; i < members.getsize(); ++i) {
+        cout << "-" << members.get(i)->getName() << " roll no: " <<
+         members.get(i)->getRoll() << "\n";
     }
-    void listallassignments(){
-        cout << "Assignments in Club " << Clubname << ":\n";
-        if (assignments.isEmpty()) {
-            cout << "  [No assignments]\n";
-            return;
-        }
-        for (int i = 0; i < assignments.getSize(); i++) {
-            cout << "  - " << assignments.get(i)->getTitle()
-                 << " (Max Score: " << assignments.get(i)->getMaxScore()
-                 << ", Deadline: " << assignments.get(i)->getDeadline() << ")\n";
-        }
-    }
+}
 
-};
+void club::addMember(Member* m) {
+    members.push_back(m);
+}
+
+void club::listallassignments() {
+    cout << "Assignments in Club " << Clubname << ":\n";
+    if (assignments.isEmpty()) {
+        cout << "  [No assignments]\n";
+        return;
+    }
+    for (int i = 0; i < assignments.getSize(); i++) {
+        cout << "  - " << assignments.get(i)->getTitle()
+             << " (Max Score: " << assignments.get(i)->getMaxScore()
+             << ", Deadline: " << assignments.get(i)->getDeadline() << ")\n";
+    }
+}
---
+++ club.h
@@ -0,0 +1,33 @@
+#ifndef CLUB_H
+#define CLUB_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Admin; 
+class Member; 
+class Assignment; 
+
+template <typename T>
+class Vector; 
+
+class club {
+    private:
+        string Clubname;
+        Admin* admin;
+        Vector<Member*> members;
+        Vector<Assignment*> assignments;
+
+    public:
+            club(string name, Admin* a) {
+                Clubname = name;
+                admin = a;
+            }
+
+        void listallmembers();
+        void listallassignments();
+        void addMember(Member* m);
+};
+
+#endif 
