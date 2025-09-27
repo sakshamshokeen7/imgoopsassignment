cbd56d3cad375c241d07c9ffb2918c7ba231fc71
APT-KK <khoslakabeer527@gmail.com>
2025-09-27 17:09:00 +0530

    fix error in student

--- headers/student.h
+++ headers/student.h
@@ -1,3 +1,29 @@
+// Student.h
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "Member.h"
+#include <string>
+
+template<typename T>
+class Vector;
+class club;
+class Assignment;
+class Submission;
+
+class Student : public Member {
+private:
+    Vector<club*> clubs;
+public:
+    // keep parameters as (int roll, std::string n, std::string pass) but ensure we call base correctly
+    Student(int roll, std::string n, std::string pass);
+    void submitAssignment(Assignment* assignment, std::string submissionDate, bool lateFlag);
+    void viewAssignments();
+    void listAllClubs() const;
+    Vector<club*>& getClubs() { return clubs; }
+};
+
+#endif // STUDENT_H
 #ifndef STUDENT_H
 #define STUDENT_H
 
